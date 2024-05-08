50d29b88bb01ab72558ce9283a4d0feec56de682
amithasanamithasan <amit35-1773@diu.edu.bd>
2024-05-08 23:30:04 +0600

    localstorage donations data add successfully add and befor data added then error massage show data alredy exsits

--- src/Pages/Home/DonationDetails/Dontationscard.jsx
+++ src/Pages/Home/DonationDetails/Dontationscard.jsx
@@ -1,13 +1,49 @@
 import { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import swal from 'sweetalert';
 const Dontationscard = ({data}) => {
     
-    const {picture, title, price, description, text_button_bg ,title_color} = data;
+    const {id,picture, title, price, description, text_button_bg ,title_color} = data;
     // console.log(data);
+
     const [clicked, setClicked] = useState(false);
+
+
     const handleDonations = () => {
-        // toast("Donation succeed !");
+
+         const addDoantionsItmsArray=[];
+        
+         const DonationsItems=JSON.parse(localStorage.getItem('Donations'))  
+
+      if(!DonationsItems)
+        {
+            addDoantionsItmsArray.push(data);
+            localStorage.setItem('Donations',JSON.stringify(addDoantionsItmsArray))
+            swal("Good job!", "Donets alredy successfully add!", "success");
+        
+      }
+      
+
+        else {
+
+            const isExits =DonationsItems.find(data=>data.id==id)
+
+     if(!isExits){
+     addDoantionsItmsArray.push(...DonationsItems,data)
+     localStorage.setItem('Donations',JSON.stringify(addDoantionsItmsArray))
+    
+       
+                }
+
+              else
+              {
+                swal("Error!", "Donets not duplicad added", "Error");
+              }
+         
+        }
+      
+      
         toast.success("Donation succeed !");
         //  {
         //  position: toast.POSITION.BOTTOM_RIGHT
@@ -22,7 +58,7 @@ const Dontationscard = ({data}) => {
             {/* //     <Helmet>
     //     <title>Donation Details: {id}</title>
     // </Helmet> */}
-       <section className="py-24">
+       <section className="py-24 ">
             <div className="container mx-auto px-5">
                 <div className="space-y-5">
                     <div className="relative">
@@ -30,10 +66,10 @@ const Dontationscard = ({data}) => {
                         before:w-full before:bottom-0">
                             <img className="w-full h-[200px] md:h-[500px] object-cover object-center" src={picture} alt={title} />
                         </div>
-                        <button disabled={clicked} onClick={handleDonations}
-                        className={`absolute bottom-3 md:bottom-[26px] z-10 left-5 px-5 py-2 text-white`} 
-                        style={clicked?{backgroundColor:'#928a8a',color:'#bab7b7',cursor:'not-allowed'}:{backgroundColor:`${text_button_bg}`}} >
-                            Donate ${price}</button>
+<button disabled={clicked} onClick={handleDonations}
+ className={`absolute bottom-3 md:bottom-[26px] z-10 left-5 px-5 py-2 text-white`} 
+ style={clicked?{backgroundColor:'#928a8a',color:'#bab7b7',cursor:'not-allowed'}:{backgroundColor:`${text_button_bg}`}} >
+ Donate ${price}</button>
                     </div>
                     <div>
                         <h2 className="text-3xl font-semibold">{title}</h2>
